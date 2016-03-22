bacca23528f287119e1ad49d73a17e7748d4a180
viomac <vmaceda_29@hotmail.com>
2016-03-22 03:46:06 -0600

    Used special attribute '*ndFor', similar to an foreach. NOTE: Is an a example of a directive, this directive extends HTML and adds extra behavior. In this case it will repeat the <li> element based on the expression assigned to it.

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -5,9 +5,15 @@ import {Component} from 'angular2/core'
     template: `
         <h2>Courses</h2>
         {{ title }}
+        <ul>
+            <li *ngFor="#course of courses">
+                {{ course }}
+            </li>
+        </ul>
     `
 })
 
 export class CoursesComponent {
     title = "The title of courses page";
+    courses = ["Course1","Course2","Course3"];
 }
