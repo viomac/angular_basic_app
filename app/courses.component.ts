b5536b64005854edac6072eab350a7e9b4e070e0
viomac <vmaceda_29@hotmail.com>
2016-03-22 03:35:57 -0600

    Used interpolation to make a one way binding in the courses component. NOTE: If the value of a property in the component changes it will be automatically refreshed in the view, this is called ONE WAY BINDING.

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -2,9 +2,12 @@ import {Component} from 'angular2/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>Courses</h2>
+        {{ title }}
+    `
 })
 
 export class CoursesComponent {
-    
+    title = "The title of courses page";
 }
