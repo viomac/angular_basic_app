36c72367c86cd709486bb68082d9c29d0526a1f6
viomac <vmaceda_29@hotmail.com>
2016-03-31 10:47:59 -0600

    PROPERTY BINDING
    We bind a property of the DOM element like 'src' for this example, to a property in our component.
    
    There are three sintaxes for property binding:
       <img src="{{ imageUrl }}" />    --> Using interpolation
       <img [src] = "imageUrl" />	--> Using square brackets
       <img bind-src = "imageUrl" />   --> Using a prefix 'bind-'
    
    We can use these conventions:
       Interpolation -> For adding dynamic values between headings, divs, spans, etc.
       Square brackets -> For property bindings.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,17 @@ import {AuthorsComponent} from './authors.component'
 @Component({
     selector: 'my-app',
     template: `
-        <h1>My First Angular 2 App</h1>
+        <h1>{{ title }}</h1>
         <courses></courses>
         <authors></authors>
+        <img src="{{ imageUrl }}" />
+        <img [src] = "imageUrl" />
+        <img bind-src = "imageUrl" />
     `,
     directives: [CoursesComponent, AuthorsComponent]
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = "My First Angular 2 App";
+    imageUrl = "http://lorempixel.com/400/200/";
+}
