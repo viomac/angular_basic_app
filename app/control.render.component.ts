d78f4b7c5294260bf98543d8a79e1c458f8f7daa
viomac <vmaceda_29@hotmail.com>
2016-04-09 18:48:50 -0500

    Controlling Rendering of HTML - ngSwitch
    We use it to display an element and its children from a set of possible elements, based on a condition.

--- app/control.render.component.ts
+++ app/control.render.component.ts
@@ -3,25 +3,40 @@ import {Component} from 'angular2/core'
 @Component ({
     selector: 'control-render',
     template: `
-        <h2>Controlling Rendering of HTML</h2>
-        <h3>*ngIF</h3>
-        <div *ngIf="courses.length > 0">
-            <p>List of courses</p>
-        </div>
-        <div *ngIf="courses.length == 0">
-            <p>You dont have any courses yet.</p>
-        </div>
+<h2>Controlling Rendering of HTML - *ngIf</h2>
+    <h3>*ngIF</h3>
+    <div *ngIf="courses.length > 0">
+        <p>List of courses</p>
+    </div>
+    <div *ngIf="courses.length == 0">
+        <p>You dont have any courses yet.</p>
+    </div>
+
+    <h3>Hidden divs</h3>
+    <div [hidden]="courses.length == 0">
+        <p>List of courses</p>
+    </div>
+    <div [hidden]="courses.length > 0">
+        <p>You dont have any courses yet.</p>
+    </div>
     
-        <h3>Hidden divs</h3>
-        <div [hidden]="courses.length == 0">
-            <p>List of courses</p>
-        </div>
-        <div [hidden]="courses.length > 0">
-            <p>You dont have any courses yet.</p>
-        </div>
+<h2>Controlling Rendering of HTML - ngWItch</h2>
+    <ul class = "nav nav-pills">
+        <li [class.active]="viewMode == 'map'">
+            <a (click)="viewMode = 'map'">Map View</a>
+        </li>
+        <li [class.active]="viewMode == 'list'">
+            <a (click)="viewMode = 'list'">List View</a>
+        </li>
+    </ul>
+    <div [ngSwitch]="viewMode">
+        <template [ngSwitchWhen]="'map'" ngSwitchDefault>Map View Content</template>
+        <template [ngSwitchWhen]="'list'">List View Content</template>
+    </div>
     `
 })
 
 export class ControlRenderComponent {
     courses = [];
+    viewMode = 'map';
 }
