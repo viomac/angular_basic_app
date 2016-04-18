f0412d55315114bfff5817e1b46df882f9cdf319
viomac <vmaceda_29@hotmail.com>
2016-04-18 01:25:44 -0500

    Building Forms with Complex Validation - Implementing Custom Validation

--- app/signup-form.component.ts
+++ app/signup-form.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {ControlGroup, Control, Validators, FormBuilder} from 'angular2/common';
+import {UsernameValidators} from './usernameValidators';
 
 @Component({
     selector: 'signup-form',
@@ -11,7 +12,10 @@ export class SignUpFormComponent {
 
     constructor(fb: FormBuilder){
         this.form = fb.group({
-            username: ['', Validators.required],
+            username: ['', Validators.compose([
+                Validators.required,
+                UsernameValidators.cannotContainSpace
+            ])],
             password: ['', Validators.required]
         });
     }
