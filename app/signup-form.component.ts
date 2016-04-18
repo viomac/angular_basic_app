ca217aed8465344ae2c24c62fe66ec1ce4428a59
viomac <vmaceda_29@hotmail.com>
2016-04-18 02:04:50 -0500

    Building Forms with Complex Validation - Async Validators

--- app/signup-form.component.ts
+++ app/signup-form.component.ts
@@ -15,7 +15,7 @@ export class SignUpFormComponent {
             username: ['', Validators.compose([
                 Validators.required,
                 UsernameValidators.cannotContainSpace
-            ])],
+            ]), UsernameValidators.shouldBeUnique],
             password: ['', Validators.required]
         });
     }
